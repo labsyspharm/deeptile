318c362b02084a82f5f30ea8296405efa665d4ed
Hung-Yi Wu <hywu0110@gmail.com>
2019-08-09 12:19:56 -0400

    modify job script according to latest needs

--- autoencoder/slurmjob_CPU.py
+++ autoencoder/slurmjob_CPU.py
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=getBound
+#SBATCH --job-name=getNorm
 #SBATCH -n 20               # Number of cores
 #SBATCH -N 1                # Ensure that all cores are on one machine
-#SBATCH -t 0-01:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-03:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p priority            # Partition to submit to
 #SBATCH --mem-per-cpu=8G    # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o joboutput_%j.out # File to which STDOUT will be written, %j inserts jobid
@@ -12,4 +12,4 @@
 
 module load gcc/6.2.0 python/3.6.0
 source /home/hw233/virtualenv/py3/bin/activate
-python get_tile_bounds.py
+python get_tile_normalizer.py -n 20
